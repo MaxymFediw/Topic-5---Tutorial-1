53bea2ce02c51e5c1e563b81067743a2ff22ea10
Maxym Fediw <362706764@GoTVDSB.ca>
2026-02-17 12:35:52 -0500

    All Done.

--- Program.cs
+++ Program.cs
@@ -4,7 +4,52 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+
+            int people = 20;
+            int cats = 30;
+            int dogs = 15;
+
+            Console.WriteLine("People: " + people + "Dogs: " + dogs + "Cats: " + cats);
+            if (people < cats)
+            {
+
+                Console.WriteLine("Too many cats! The world is doomed!");
+            
+            
+            
+            }
+            if (people > cats) 
+            {
+                Console.WriteLine("Not many cats! The world is saved!");
+            
+            }
+            if (people < dogs) 
+            {
+                Console.WriteLine("The world is drooled on!");
+            
+            }
+            if (people > dogs) 
+            {
+                Console.WriteLine("Te world is dry!");
+            }
+            Console.WriteLine("Press ENTER To Continue");
+            Console.ReadLine();
+            Console.Clear();
+            dogs += 5;   //Add 5 dogs. What does doegs equal now?
+            Console.WriteLine("People: " + people + "Dogs: " + dogs + "Cats: " + cats);
+            if (people >= dogs) 
+            {
+                Console.WriteLine("Humans are greater or equal to dogs.");
+            }
+            if (people <= dogs) 
+            {
+                Console.WriteLine("Humans are less than or equal to dogs.");
+            }
+            if (people == dogs) 
+            {
+                Console.WriteLine("People are dogs.");
+            }
+
         }
     }
 }
